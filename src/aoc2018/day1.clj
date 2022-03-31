9b60b0db9354124bb31ffa5a1e66eac14f21eb98
Donghyuk <donghyuk.lee@greenlabs.co.kr>
2022-03-31 13:53:13 +0900

    Refectoring day1 codes.

--- src/aoc2018/day1.clj
+++ src/aoc2018/day1.clj
@@ -1,21 +1,41 @@
 (ns aoc2018.day1
   (:require
-    [clojure.java.io :as io]))
+    [clojure.string :as string]))
+
+; FIXME: slurp로 한번 만들어 보자. 완료
 
 (defn read-file [file-name]
-  (->> (io/reader file-name)
-       line-seq))
+  (->> (slurp file-name)
+       string/split-lines))
+
+(def input
+  (->> (read-file "day1_input.txt")
+       (map #(Integer/parseInt %))))
 
 ; Part One
 
-(def input (map #(Integer/parseInt %) (read-file "day1_input.txt")))
-(println (reduce + input))
+(defn part1 [input]
+  (->> input
+       (reduce +)))
 
-; Part Two
+; Part Two - Loop
 
-(println (reduce (fn [history currentSum]
-                   (if (history currentSum)
-                     (reduced currentSum)
-                     (conj history currentSum)))
-                 #{}
-                 (reductions + (cycle input))))
+(defn part2-loop [input]
+  (loop [history #{}
+         before 0
+         inputs (seq input)]
+    (let [sum (+ before (first inputs))]
+      (if (not (history sum))
+        (recur (conj history sum) sum (if (nil? (next inputs)) input (next inputs)))
+        sum
+        ))))
+
+; Part Two - reduce
+
+(defn part2-reduce [input]
+  (reduce (fn [history currentSum]
+            (if (history currentSum)
+              (reduced currentSum)
+              (conj history currentSum)))
+          #{}
+          (reductions + (cycle input))))
