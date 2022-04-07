be9938089aeaa5a43a0a0d7e5ac40f536b3a3d13
Donghyuk <donghyuk.lee@greenlabs.co.kr>
2022-04-07 21:21:50 +0900

    Reflect code.

--- src/aoc2018/day5.clj
+++ src/aoc2018/day5.clj
@@ -9,50 +9,59 @@
 
 ; Logic
 
-(defn pair-char?
-  "두개의 string이 같은지 비교한다. 단 aa AA는 아니고 aA나 Aa일 경우에는 참이다."
-  [str1, str2]
-  (and str1 str2
-       (not= str1 str2)
-       (= (string/lower-case str1) (string/lower-case str2))))
+(defn react-rules
+  [[\a \A]
+   [\b \B]])
 
-(defn get-compress-text
-  "전체 스트링을 하나씩 전달받아 최종 스트링을 만든다."
-  [remain-strings current-string]
-  (let [last-string (last remain-strings)]
+(defn reactive-units?
+  "두개의 unit이 서로 반응하는지 검사한다."
+  [unit1, unit2]
+  (and unit1 unit2
+       (not= unit1 unit2)
+       (= (string/lower-case unit1) (string/lower-case unit2))))
+
+(defn react
+  "polymer의 unit을 반응 시킨다."
+  [polymer unit]
+  (let [last-unit (last polymer)]
     (cond
       ; 첫 진입시
-      (empty? remain-strings) (str current-string)
+      (empty? polymer) (str unit)
       ; 마지막 string이 previous와 경우에는 previous의 마지막을 제거한다.
-      (pair-char? current-string last-string) (subs remain-strings 0 (dec (count remain-strings)))
+      (reactive-units? unit last-unit) (subs polymer 0 (dec (count polymer)))
       ; 마지막 string이 previous와 같지 않을 경우에는 마지막에 추가한다.
-      :else (str remain-strings (str current-string)))))
+      :else (str polymer (str unit)))))
 
-(defn get-chars
+(defn get-units
   "입력 string에서 사용하는 유일한 char을 return 한다. 소문자로 변환한다."
   [data]
   (->> data
        string/lower-case
        set))
 
-(defn get-optimization-string
+(defn get-optimization-polymer
   "입력 string에서 remove-string을 입력받아 replace '' 하여준다."
   [data remove-string]
   (string/replace data (re-pattern (str "(?i)" remove-string)) ""))
 
-(defn data->optimization-texts
+(defn data->optimization-polymer
   "입력 string에서 요소로 존재하는 string을 제거한 문자열의 Array로 반환한다."
   [data]
   (->> data
-       get-chars
+       get-units
        (common/debug "(get-optimization-strings) Chars")
-       (map #(get-optimization-string data %))))
+       (map #(get-optimization-polymer data %))))
 
-(defn text->compress-text
+(defn text->react
   "text를 compress text로 변경하여 줍니다."
   [string]
   (->> string
-       (reduce get-compress-text "")))
+       (reduce react "")))
+
+(defn chain-react
+  "polymer의 연쇄 반응 후 크기를 줄인다."
+  [polymer]
+  (reduce react "" polymer))
 
 ; Solution
 
@@ -60,15 +69,15 @@
   "연쇄작용이 일어나는 부분을 제거하고 남는 문자를 만든다."
   [data]
   (->> data
-       (reduce get-compress-text "")
+       chain-react
        count))
 
 (defn part2
   "입력에서 제거 할 경우에 길이가 가장 짤아질때 길이를 구한다."
   [data]
   (->> data
-       data->optimization-texts
-       (map text->compress-text)
+       data->optimization-polymer
+       (map text->react)
        (map count)
        sort
        first))
@@ -84,8 +93,8 @@
 
 ; Test
 (comment
-  (data->optimization-texts test-data)
-  (pair-char? "A" "A")
+  (data->optimization-polymer test-data)
+  (reactive-units? "A" "A")
   (string/lower-case \A)
   (and nil nil nil)
   (string/replace "dabAcCaCBAcCcaDA" #"(?i)c" "")
