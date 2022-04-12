810443c44c332e8bd95b801852179577f788528a
Donghyuk <donghyuk.lee@greenlabs.co.kr>
2022-04-12 12:08:04 +0900

    Add comment.

--- src/aoc2018/day7.clj
+++ src/aoc2018/day7.clj
@@ -17,37 +17,41 @@
                    re-find
                    rest)))))
 
-(defn get-units
+(defn key-set
+  "Keys -> Key #{} 형태로 변환한다."
+  [keys]
+  (->> keys
+       (reduce (fn [acc value]
+                 (assoc acc value #{}))
+               {})))
+
+(defn route-init
   "경로에 존재하는 데이터를 모두 받아들여서 유일한 키의 HashMap으로 return하는 function
   Input : ((C A) (C F) (A B) (A D) (B E) (D E) (F E))
   Output : {E #{}, C #{}, F #{}, B #{}, A #{}, D #{}}"
   [data]
   (->> data ; ((C A) (C F) (A B) (A D) (B E) (D E) (F E))
-       flatten ; (C A C F A B A D B E D E F E). 모든것을 다 벗기고 Flat 하게 만든다.
-       set ; #{E C F B A D}
-       (reduce (fn [acc value]
-                 (assoc acc value #{}))
-               {}))) ; {E #{}, C #{}, F #{}, B #{}, A #{}, D #{}}
+       flatten ; (C A C F A B A D B E D E F E). Flat 하게 만든다.
+       key-set)) ; {E #{}, C #{}, F #{}, B #{}, A #{}, D #{}}
 
-(defn grouping
+(defn route-grouping
   "Before와 연결될수 있는 After들을 만들어주는 function"
   [data]
   (->> data ; ((C A) (C F) (A B) (A D) (B E) (D E) (F E))
-       (reduce (fn [acc [from to]]
-                 (update acc to #(conj % from)))
-               (get-units data)))) ; {E #{F B D}, C #{}, F #{C}, B #{A}, A #{C}, D #{A}}
+       (reduce (fn [acc [before next]]
+                 (update acc next #(conj % before))) ; {E #{F B D}, C #{}, F #{C}, B #{A}, A #{C}, D #{A}}
+               (route-init data)))) ; {E #{}, C #{}, F #{}, B #{}, A #{}, D #{}}
 
 (defn find-next-unit
   "Before 값이 없는 값들중에 key가 빠른 값을 찾는다."
   [groups]
   (->> groups ; {E #{F B D}, C #{}, F #{C}, B #{A}, A #{C}, D #{A}}
-       (filter (fn [[_ before-values]] ; GROUP #{BEFOREs...}
-                 (empty? before-values)))
+       (filter #(empty? (val %))) ; Value가 1개라면 자신이 다음 후보다.
        (sort-by key) ; ([F ()] [A ()]) -> ([A ()] [F ()])
        first ; [A ()]
        key)) ; A
 
-(defn remove-done-work
+(defn remove-done-route
   "Order에 사용된 value를 제거하여 준다."
   [next-work groups]
   (->> groups ; {E #{F B D}, F #{C}, B #{A}, A #{C}, D #{A}}
@@ -55,35 +59,35 @@
               [group (remove #(= % next-work) before-values)])) ; ([E (F B D)] [F ()] [B (A)] [A ()] [D (A)])
        (into {}))) ; {E (F B D), F (), B (A), A (), D (A)}
 
-(defn work-order
+(defn route-order
   "Work order를 구하는 function"
   [groups]
   (->> groups
-       (reduce (fn [[groups work-plan] _]
+       (reduce (fn [[groups route-plan] _]
                  (let [next-unit (find-next-unit groups)
-                       work-plan (conj work-plan next-unit)
-                       groups (remove-done-work next-unit
-                                                (dissoc groups next-unit))]
-                   [groups work-plan]))
+                       route-plan (conj route-plan next-unit)
+                       groups (remove-done-route next-unit
+                                                 (dissoc groups next-unit))]
+                   [groups route-plan]))
                [groups []])
        last))
 
-(defn join-units
-  [units]
-  (s/join units))
+(defn join-order
+  [order]
+  (s/join order))
 
 (comment
   (->> test-file-name
        common/read-file
        lines->from-tos
-       grouping
-       reduce-works
-       join-units)
+       route-grouping
+       route-order
+       join-order) ; "CABDFE"
   (->> file-name
        common/read-file
        lines->from-tos
-       grouping
-       reduce-works
-       join-units)) ; "GLMVWXZDKOUCEJRHFAPITSBQNY"
+       route-grouping
+       route-order
+       join-order)) ; "GLMVWXZDKOUCEJRHFAPITSBQNY"
 
 
