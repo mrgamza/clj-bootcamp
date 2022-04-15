7f1aec20aeef748edeb03d68f1a245495cb783da
Donghyuk <donghyuk.lee@greenlabs.co.kr>
2022-04-15 10:17:58 +0900

    Make 2020 day1

--- src/aoc2020/day1.clj
+++ src/aoc2020/day1.clj
@@ -10,51 +10,53 @@
        common/read-file
        (map #(Integer/parseInt %))))
 
-(defn find-two-sum
-  [{:keys [target mores]}]
-  (let [second (->> mores
-                    (filter #(= 2020 (+ target %)))
-                    first)]
-    (when (not (nil? second)) [target second])))
-
-(defn divide-target-mores
-  [data]
-  {:target (first data)
-   :mores (next data)})
-
-(defn find-combination
-  [{:keys [target-mores]}]
-  {:target-mores (divide-target-mores (target-mores :mores))
-   :combination (find-two-sum target-mores)})
-
-(defn has-combination?
-  [data]
-  (let [combination (data :combination)]
-    (< (count combination) 2)))
-
-(defn part1
-  [data]
-  (let [init (divide-target-mores data)]
-    (->> {:target-mores init
-          :combination '[]}
-         (iterate find-combination)
-         (drop-while has-combination?)
-         first
-         :combination
-         (apply *))))
-
 ; Part1
 
-(comment
-  (->> test-file
+(defn combination-2-sum
+  [numbers]
+  (let [number-count (count numbers)]
+    (for [index1 (range number-count)
+          index2 (range number-count)
+          :let [target (nth numbers index1)
+                search (nth numbers index2)]
+          :when (and
+              (< index1 index2)
+              (= (+ target search) 2020))]
+      (* target search))))
+
+(defn part1
+  [file-name]
+  (->> file-name
        read-file
-       part1) ; 514579
-  (->> real-file
+       combination-2-sum
+       first))
+
+; Part2
+
+(defn combination-3-sum
+  [numbers]
+  (let [number-count (count numbers)]
+    (for [index1 (range number-count)
+          index2 (range number-count)
+          index3 (range number-count)
+          :let [target (nth numbers index1)
+                search1 (nth numbers index2)
+                search2 (nth numbers index3)]
+          :when (and
+                  (< index1 index2)
+                  (< index2 index3)
+                  (= (+ target search1 search2) 2020))]
+      (apply * [target search1 search2]))))
+
+(defn part2
+  [file-name]
+  (->> file-name
        read-file
-       part1)) ; 719796
+       combination-3-sum
+       first))
 
 (comment
-  (->> test-file
-       read-file
-       divide-target-mores
-       find-two-sum))
+  (part1 test-file) ; 514579
+  (part1 real-file) ; 719796
+  (part2 test-file) ; 241861950
+  (part2 real-file)) ; 144554112
